6b2e5a73fcd70a8775eed51fb238fed99a7d60b3
zbyso23 <z.lipka@seznam.cz>
2016-10-07 00:44:21 +0200

    Update MD.js

--- MD.js
+++ MD.js
@@ -1,4 +1,4 @@
-// MD, copyright (c) by Zbigniew Lipka
+// MD.js, copyright (c) by Zbigniew Lipka
 // Distributed under an GNU GENERAL PUBLIC LICENSE version 3: https://github.com/zbyso23/MD/blob/master/LICENSE
 
 var MD, MD_ADDONS;
